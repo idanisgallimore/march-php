f5cfc27d5b4accae83f2cfbcaa057f121dce059f
idanisg <idanisgallimore@gmail.com>
2019-03-26 05:45:20 -0400

    create user account

---
+++ includes/addUser.inc.php
@@ -0,0 +1,86 @@
+<?php 
+include_once('library/connectToDb.php');
+$con = connectToDb();
+$name = htmlspecialchars($_REQUEST['name']);
+$last_name = htmlspecialchars($_REQUEST['name2']);
+$password = htmlspecialchars($_REQUEST['password']);
+$password2 = htmlspecialchars($_REQUEST['password2']);
+$email = htmlspecialchars($_REQUEST['email']);
+$badUser = 0;
+
+if (empty($password) || empty($password2)) {
+    echo "<div class=\"flex msg-container fail-msg-container\">
+        <h2 class =\"fail-msg msg\">Please, enter a password. Make sure to confirm password.</h2>
+        <a href=\"index.php?page=createaccount&email=$email\" class=\"page-link\">Try Again</a>
+        </div>";
+    include("components/footer.php");
+    $badUser = 1;
+    exit;
+}
+if (strlen($password) < 8) {
+    echo "<div class=\"flex msg-container fail-msg-container\">
+        <h2 class =\"fail-msg msg\">Passwords need to be atleast 8 characters long.</h2>
+        <a href=\"index.php?page=createaccount&email=$email\" class=\"page-link\">Try Again</a>
+        </div>";
+    include("components/footer.php");
+    $badUser = 1;
+    exit;
+}
+if (empty($email) || !filter_var($email, FILTER_VALIDATE_EMAIL)) {
+    echo "<div class=\"flex msg-container fail-msg-container\">
+        <h2 class =\"fail-msg msg\">Please, enter a valid email.</h2>
+        <a href=\"index.php?page=createaccount&email=$email\" class=\"page-link\">Try Again</a>
+        </div>";
+    include("components/footer.php");
+    $badUser = 1;
+    exit;
+}
+if (empty($name) || empty($last_name)) {
+    echo "<div class=\"flex msg-container fail-msg-container\">
+        <h2 class =\"fail-msg msg\">Please, enter your name.</h2>
+        <a href=\"index.php?page=createaccount&email=$email\" class=\"page-link\">Try Again</a>
+        </div>";
+    include("components/footer.php");
+    $badUser = 1;
+    exit;
+}
+if ($password !== $password2) {
+    echo "<div class=\"flex msg-container fail-msg-container\">
+            <h2 class =\"fail-msg msg\">Passwords don't match!</h2>
+            <a href=\"index.php?page=createaccount&email=$email\" class=\"page-link\">Try Again</a>
+            </div>";
+    include("components/footer.php");
+    $badUser = 1;
+    exit;
+}
+if ($badUser === 0) {
+    $email = strtolower($email);
+    $query = "SELECT * FROM customers WHERE email = '$email'";
+    $result = mysqli_query($con, $query);
+    $rowcount = mysqli_num_rows($result);
+    if ($rowcount > 0) {
+        echo "<div class=\"flex msg-container fail-msg-container\">
+            <h2 class =\"fail-msg msg\">An account with this email already exists.</h2>
+            <a href=\"index.php?page=createaccount&email=$email\" class=\"page-link\">HOME</a>
+            </div>";
+    } else {
+        $password = password_hash($password, PASSWORD_DEFAULT);
+        $email = strtolower($email);
+        $query = "INSERT INTO customers(first_name, last_name, email, password) VALUES('$name','$last_name', '$email', '$password')";
+        $result = mysqli_query($con, $query);
+        $error = mysqli_error($con);
+        if ($result) {
+            // Change this when you add user cookies/sessions
+            echo "<div class=\"flex msg-container fail-msg-container\">
+                <h2 class =\"fail-msg msg\">Success your account was created</h2>
+                <a href=\"index.php?page=loginPage\" class=\"page-link\">Log in</a>
+                </div>";
+        } else {
+            echo "<div class=\"flex msg-container fail-msg-container\">
+                    <h2 class =\"fail-msg msg\">Something went wrong. Try again later. : $error</h2>
+                    <a href=\"index.php?page=createaccount&email=$email\" class=\"page-link\">Try Again</a>
+                    </div>";
+        }
+    }
+}
+ 
--- includes/createaccount.inc.php
+++ includes/createaccount.inc.php
@@ -1,14 +1,16 @@
 <div class="register-page flex">
     <form class="register__form flex" action="index.php" method="post">
         <h3 class="ls6__title">Register</h3>
-        <input class="email-list__userin email-list__userin1" name="name" type="text" placeholder="Enter Your Name">
+        <input class="email-list__userin email-list__userin1" name="name" type="text" placeholder="Enter Your First Name">
+        <input class="email-list__userin email-list__userin1" name="name2" type="text" placeholder="Enter Your Last Name">
         <?php 
-        $email = $_REQUEST['email'];
-        echo "<input class=\"email-list__userin email-list__userin1\" name=\"email\" type=\"text\" value=\"$email\">"
+            $email = $_REQUEST['email'];
+            echo "<input class=\"email-list__userin email-list__userin1\" name=\"email\" type=\"text\" value=\"$email\">"
         ?>
         <input class="email-list__userin email-list__userin2" name="password" type="password" placeholder="Enter Password">
-        <input type="hidden" name="page" value="login">
-        <input class="btn btn-short login-btn btn-black" type="submit" value="Sign in">
+        <input class="email-list__userin email-list__userin2" name="password2" type="password" placeholder="Confirm Password">
+        <input type="hidden" name="page" value="addUser">
+        <input class="btn btn-long login-btn btn-black" type="submit" value="Create Account">
     </form>
 </div>
 <?php include('components/divider.php') ?> 
