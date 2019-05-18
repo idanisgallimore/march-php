787fed6224f9ac9d0d654730e9afe4dcb189c2bf
idanisg <idanisgallimore@gmail.com>
2019-05-18 07:51:08 -0400

    checkout

--- includes/checkoutDone.inc.php
+++ includes/checkoutDone.inc.php
@@ -2,22 +2,33 @@
 // DB connection 
 include_once('library/connectToDb.php');
 $con = connectToDb();
-// values from form
-$fName = $_REQUEST['fName'];
-$lName = $_REQUEST['lName'];
-$address = $_REQUEST['address'];
-$city = $_REQUEST['city'];
-$state = $_REQUEST['state'];
-$zipcode = $_REQUEST['zipcode'];
-$user = $_REQUEST['userid'];
+
 
 if (isset($_SESSION['user'])) {
-    $query = "INSERT INTO customers(first_name, last_name, address, city, state, zip_code) VALUES('$fName', '$lName', '$address', '$city', '$state', '$zipcode') WHERE customer_id = '$user'";
-    $result = mysqli_query($con, $query);
-    if($result){
-        echo "it worked";
+    $fName = $_REQUEST['fName'];
+    $lName = $_REQUEST['lName'];
+    $address = $_REQUEST['address'];
+    $city = $_REQUEST['city'];
+    $state = $_REQUEST['state'];
+    $zipcode = $_REQUEST['zipcode'];
+    $user = $_REQUEST['userid'];
+
+    $query = "UPDATE customers SET first_name = '$fName', last_name = '$lName', address = '$address', city = '$city', state = '$state', zip_code = '$zipcode' WHERE customer_id = $user";
+    $result = mysqli_query($con, $query) or die(mysqli_error($con));
+    if ($result) {
+        unset($_SESSION['cart']); 
+        echo "<div class=\"flex msg-container fail-msg-container\">
+        <h2 class =\"fail-msg msg\">Order Confirmed</h2>
+        </div>";
+    } else {
+        echo "<div class=\"flex msg-container fail-msg-container\">
+        <h2 class =\"fail-msg msg\">Order not processed.</h2>
+        </div>";
     }
- }
-
-
+} else {
+    unset($_SESSION['cart']); 
+    echo "<div class=\"flex msg-container fail-msg-container\">
+    <h2 class =\"fail-msg msg\">Order Confirmed</h2>
+    </div>";
+}
 ?>
