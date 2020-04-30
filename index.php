754d38e2459e7331507c49e932dd3fd6cfbb8dc5
idanisg <idanisgallimore@gmail.com>
2020-04-30 12:38:08 -0400

    first commit

--- index.php
+++ index.php
@@ -10,7 +10,7 @@ session_start();
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <link rel="stylesheet" href="styles/normalize.css">
-    <link rel="stylesheet" href="styles/style.css">
+    <link rel="stylesheet" href="styles/min.css">
     <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.2/css/all.css" integrity="sha384-fnmOCqbTlWIlj8LyTjo7mOUStjsKC4pOpQbqyi7RrhN7udi9RwhKkMHpvLbHG9Sr" crossorigin="anonymous">
     <title>Parmlèes Official</title>
 </head>
--- library/connectToDb.php
+++ library/connectToDb.php
@@ -1,6 +1,8 @@
 <?php 
     function connectToDb(){
-        $con = mysqli_connect("localhost", "idanis", "test", "parmlees_store") or die("did not connect");
+        // $con = mysqli_connect("localhost", "idanis", "test", "parmlees_store") or die("did not connect");
+        // return $con;
+        $con = mysqli_connect("localhost", "idanisga_test", "vV,ClgAfiG&L", "idanisga_store") or die("did not connect");
         return $con;
     }
 ?>
